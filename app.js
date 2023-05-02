94f47d3aba567ad20be24e372d0391bf84ada1ed
Elijah_Fabon <efabon2@my.bcit.ca>
2023-05-01 17:53:22 -0700

    404 page

--- app.js
+++ app.js
@@ -218,6 +218,10 @@ app.get("/logout", (req, res) => {
   res.send(html);
 });
 
+app.get('*', (req, res) => {
+  res.status(404).send('<h1> 404 Page not found</h1>');
+});
+
 // only for admins
 const protectedRouteForAdminsOnlyMiddlewareFunction = async (req, res, next) => {
   try {
